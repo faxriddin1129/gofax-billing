5a7605eeaf898fa5207fe41ecea4a7f0c212b2dc
faxbob1129 <fakhriddin1129@gmail.com>
2025-07-08 14:33:30 +0500

    start payme

--- pkg/payme/init.go
+++ pkg/payme/init.go
@@ -8,7 +8,6 @@ var (
 	PROD_KEY    string
 	TEST_KEY    string
 	SERVICE_URL string
-	CACHE_ID    string
 	MERCHANT_ID string
 )
 
@@ -16,7 +15,6 @@ func LoadEnv() {
 	PROD_KEY = env.GetEnv("PAYME_PROD_KEY")
 	TEST_KEY = env.GetEnv("PAYME_TEST_KEY")
 	SERVICE_URL = env.GetEnv("PAYME_SERVICE_URL")
-	CACHE_ID = env.GetEnv("PAYME_CACHE_ID")
 	MERCHANT_ID = env.GetEnv("PAYME_MERCHANT_ID")
 }
 
