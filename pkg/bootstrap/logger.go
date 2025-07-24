b5469b8fe9d1eb8c90bd27893a40a85465ccd489
faxbob1129 <fakhriddin1129@gmail.com>
2025-07-24 12:48:28 +0500

    deployment server

--- pkg/bootstrap/logger.go
+++ pkg/bootstrap/logger.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
@@ -9,18 +10,54 @@ import (
 	"time"
 )
 
-func SetupErrorLogger() gin.HandlerFunc {
-	today := time.Now().Format("2006-01-02")
-	logFile := filepath.Join("storage", "logs", today+"_error.log")
+func RequestResponseLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
 
-	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(fmt.Sprintf("Log file not open: %v", err))
+		bodyBytes, _ := io.ReadAll(c.Request.Body)
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		today := time.Now().Format("2006-01-02")
+		logFile := filepath.Join("storage", "logs", today+"_request.log")
+		f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(fmt.Sprintf("Could not open log file: %v", err))
+		}
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+
+			}
+		}(f)
+
+		writer := &bodyWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+		c.Writer = writer
+
+		c.Next()
+
+		log := fmt.Sprintf(
+			"[%s] %s %s %d %s\nRequestBody: %s\nResponseBody: %s\n\n",
+			time.Now().Format("15:04:05"),
+			c.Request.Method,
+			c.Request.URL.String(),
+			c.Writer.Status(),
+			time.Since(start),
+			string(bodyBytes),
+			writer.body.String(),
+		)
+		_, err = f.WriteString(log)
+		if err != nil {
+			return
+		}
 	}
+}
 
-	return gin.CustomRecoveryWithWriter(io.MultiWriter(f, os.Stderr), func(c *gin.Context, recovered interface{}) {
-		c.AbortWithStatusJSON(500, gin.H{
-			"error": "Internal Server Error",
-		})
-	})
+type bodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w *bodyWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
 }
